7ebf76c28daeb9259e9113e43aa0847dcff4ebd2
smenard <smenard@student.42lyon.fr>
2026-06-29 20:28:58 +0200

    wip: json validation

--- src/models/arguments.py
+++ src/models/arguments.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator, ValidationError
+import json
 
 
 class Arguments(BaseModel):
@@ -10,3 +11,16 @@ class Arguments(BaseModel):
     )
     output: str = Field(min_length=6, default="function_calls.json")
     model: str = Field(min_length=3, default="Qwen/Qwen3-0.6B")
+
+
+    @model_validator(mode="after")
+    def validate_model(self) -> "Arguments":
+        try:
+            json.loads(self.functions_definition)
+        except json.JSONDecodeError as e:
+            raise ValueError(f"Invalid json format for file {self.functions_definition}: {e}")
+        try:
+            json.loads(self.input)
+        except json.JSONDecodeError as e:
+            raise ValueError(f"Invalid json format for file {self.input}: {e}")
+        return self
