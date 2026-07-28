83709858ecd71ceb2c6dc4fd14618d53060ba352
ardsimm <floogles71@gmail.com>
2026-07-28 14:49:37 +0200

    fix: read from vocab file to get string end sequences in StringState

--- src/state/__string_state.py
+++ src/state/__string_state.py
@@ -1,5 +1,11 @@
-from src.state.state import StateStage
-from . import State
+import json
+from typing import Dict, List
+
+from llm_sdk import Small_LLM_Model
+
+from src.generate.generation_error import GenerationError
+
+from .state import StateStage, State
 
 
 class StringStateStage(StateStage):
@@ -12,6 +18,19 @@ class StringState(State):
 
     def __init__(self) -> None:
         super().__init__()
+        model = Small_LLM_Model()
+        string_end_sequences: List[int] = []
+        try:
+            with open(model.get_path_to_vocab_file()) as vocab_file:
+                vocab_dict: Dict[str, int] = json.loads(vocab_file.read())
+            for key in vocab_dict.keys():
+                if '"' in key:
+                    string_end_sequences.append(vocab_dict[key])
+        except FileNotFoundError as e:
+            raise GenerationError("Failed to open vocab file: " + e)
+        except json.JSONDecodeError as e:
+            raise GenerationError("Failed to parse vocab file: " + e)
+
         self._stages = [
             StringStateStage.INITIAL,
             StringStateStage.DATA,
@@ -24,5 +43,5 @@ class StringState(State):
         }
         self._stage_transition_tokens = {
             StringStateStage.INITIAL: [],
-            StringStateStage.DATA: self._tokenizer.encode('"').tolist()[0],
+            StringStateStage.DATA: string_end_sequences,
         }
