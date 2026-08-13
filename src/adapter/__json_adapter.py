2123efb534e2d0a1150cf188561df81123f31e68
ardsimm <floogles71@gmail.com>
2026-08-13 15:20:39 +0200

    feat: write result to output file

--- src/adapter/__json_adapter.py
+++ src/adapter/__json_adapter.py
@@ -5,8 +5,6 @@ import json
 class JSONAdapter(Adapter):
 
     def serialize(self, value: object) -> str:
-        if not isinstance(value, dict):
-            raise ValueError("Cannot serialize non-dict object to JSON")
         return json.dumps(value)
 
     def parse(self, data: str) -> object:
