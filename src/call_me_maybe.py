80aedb80b744d5a2d7b43eb6abe6f61312841af7
smenard <smenard@student.42lyon.fr>
2026-08-18 17:16:47 +0200

    refacto(call_me_maybe): Split method run

--- src/call_me_maybe.py
+++ src/call_me_maybe.py
@@ -1,6 +1,6 @@
 from src.generate import GeneratorFactory
 from src.generate import GenerationError
-from src.parsing import ParserFactory, Parser
+from src.parsing import ParserFactory
 from src.models import Arguments, Context, OutputItem, ParameterType
 from src.adapter import AdapterFactory, AdapterType, SerializationException
 from src.parsing import ParsingError, ParsingValidationError
@@ -10,85 +10,105 @@ import os
 
 
 class CallMeMaybe:
+
     @staticmethod
-    def run() -> None:
-        parser: Parser = ParserFactory.get_instance()
+    def __get_arguments() -> Arguments:
+        parser = ParserFactory.get_instance()
+        return parser.parse(sys.argv[1:])
+
+    @staticmethod
+    def __get_context(arguments: Arguments) -> Context:
+        return Context(arguments)
+
+    @staticmethod
+    def __process_prompt(prompt: str, context: Context) -> OutputItem:
+        generator = GeneratorFactory.get_instance()
+        item: OutputItem = {"prompt": "", "name": "", "parameters": {}}
+        item["prompt"] = prompt
+
+        print(
+            "=====================================================",
+            "=====================================================",
+            f"Generating for prompt: {prompt}",
+            "=====================================================",
+            "=====================================================",
+            sep="\n",
+        )
+
+        name = generator.generate_name(prompt, context.functions)
+
+        filtered_functions = [
+            function
+            for function in context.functions
+            if function.name == name
+        ]
+        if not len(filtered_functions):
+            raise GenerationError(f"Invalid function name: [{name}]")
+        print(f"Generated name: [{name}]")
+
+        picked_function = filtered_functions[0]
+        item["name"] = picked_function.name
+        parameters = generator.generate_parameters(
+            prompt, picked_function
+        )
+        print("Generated parameters:")
+        for parameter in parameters:
+            print(
+                f"- {parameter.name} <{parameter.type.value}>:",
+                f"[{parameter.value}]",
+            )
+            parsed_parameter: Union[int, float, str, bool]
+            assert parameter.value is not None
+            if parameter.type == ParameterType.INT:
+                parsed_parameter = int(parameter.value)
+            elif parameter.type == ParameterType.FLOAT:
+                parsed_parameter = float(parameter.value)
+            elif parameter.type == ParameterType.BOOL:
+                parsed_parameter = bool(parameter.value)
+            else:
+                parsed_parameter = parameter.value
+            item["parameters"][parameter.name] = parsed_parameter
+        return item
+
+    @classmethod
+    def __process_prompts(cls, context: Context) -> List[OutputItem]:
+        items: List[OutputItem] = []
+
+        for prompt in context.prompts:
+            try:
+                items.append(cls.__process_prompt(prompt, context))
+            except GenerationError as err:
+                print(f"Error while generating prompt {prompt}:\n{err}")
+                items.append({"prompt": "", "name": "", "parameters": {}})
+                continue
+        return items
+
+    @staticmethod
+    def __write_output(items: List[OutputItem], arguments: Arguments) -> None:
+        adapter = AdapterFactory.get_instance(AdapterType.JSON)
+        print("Writing result to output file...")
+        serialized = adapter.serialize(items)
+        os.makedirs("data/output", exist_ok=True)
+        with open(
+            os.path.join("data/output", arguments.output), mode="w"
+        ) as output_file:
+            output_file.write(serialized)
+
+    @classmethod
+    def run(cls) -> None:
         try:
-            arguments: Arguments = parser.parse(sys.argv[1:])
+            arguments: Arguments = cls.__get_arguments()
         except (ParsingValidationError, ParsingError):
             print("Parsing failed")
             return
-        context = Context(arguments)
-        generator = GeneratorFactory.get_instance()
-        items: List[OutputItem] = []
-        for prompt in context.prompts:
-            item: OutputItem = {"prompt": "", "name": "", "parameters": {}}
-            item["prompt"] = prompt
-            print(
-                "=====================================================",
-                "=====================================================",
-                f"Generating for prompt: {prompt}",
-                "=====================================================",
-                "=====================================================",
-                sep="\n",
-            )
-            try:
-                name = generator.generate_name(prompt, context.functions)
-                filtered_functions = [
-                    function
-                    for function in context.functions
-                    if function.name == name
-                ]
-                if not len(filtered_functions):
-                    raise GenerationError(f"Invalid function name: [{name}]")
-            except GenerationError:
-                print("Name generation failed. continuing...")
-                items.append(item)
-                continue
-            print(f"Generated name: [{name}]")
-            picked_function = filtered_functions[0]
-            item["name"] = picked_function.name
-            try:
-                parameters = generator.generate_parameters(
-                    prompt, picked_function
-                )
-            except GenerationError:
-                print("Parameters generation failed. continuing...")
-                items.append(item)
-                continue
-            print("Generated parameters:")
-            for parameter in parameters:
-                print(
-                    f"- {parameter.name} <{parameter.type.value}>:",
-                    f"[{parameter.value}]",
-                )
-                parsed_parameter: Union[int, float, str, bool]
-                assert parameter.value is not None
-                if parameter.type == ParameterType.INT:
-                    parsed_parameter = int(parameter.value)
-                elif parameter.type == ParameterType.FLOAT:
-                    parsed_parameter = float(parameter.value)
-                elif parameter.type == ParameterType.BOOL:
-                    parsed_parameter = bool(parameter.value)
-                else:
-                    parsed_parameter = parameter.value
-                item["parameters"][parameter.name] = parsed_parameter
-                items.append(item)
-        adapter = AdapterFactory.get_instance(AdapterType.JSON)
-        print("Writing result to output file...")
+        context = cls.__get_context(arguments)
+        items: List[OutputItem] = cls.__process_prompts(context)
         try:
-            serialized = adapter.serialize(items)
-        except SerializationException:
-            print("Result serialization failed...")
+            cls.__write_output(items, arguments)
+        except SerializationException as e:
+            print(f"Failed to serialize result: {e}")
             return
-        os.makedirs("data/output", exist_ok=True)
-        output_file_path = os.path.join("/data/output", arguments.output)
-        try:
-            with open(
-                output_file_path, mode="w"
-            ) as output_file:
-                output_file.write(serialized)
-        except IOError:
-            print("Failed to write output file.")
+        except IOError as e:
+            print(f"Failed to write output file: {e}")
             return
         print("Done ! :3")
