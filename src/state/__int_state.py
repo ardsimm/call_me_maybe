c4b441f99e29dc0b7917e4ba02061d6210244ed2
ardsimm <floogles71@gmail.com>
2026-08-24 17:06:59 +0200

    fix: possible infinite generation of - with int parameters

--- src/state/__int_state.py
+++ src/state/__int_state.py
@@ -6,21 +6,25 @@ from .state import StateStage, State
 
 class IntStateStage(StateStage):
     INITIAL = 0
-    DATA = 1
-    TERMINAL = 2
+    POST_MINUS = 1
+    POST_DIGIT = 2
+    TERMINAL = 3
 
 
 class IntState(State):
+
+    minus_token: List[int]
 
     def __init__(self) -> None:
         super().__init__()
         self._stages = [
             IntStateStage.INITIAL,
-            IntStateStage.DATA,
+            IntStateStage.POST_MINUS,
+            IntStateStage.POST_DIGIT,
             IntStateStage.TERMINAL,
         ]
 
-        minus_token = self._tokenizer.encode("-").tolist()[0]
+        self.minus_token = self._tokenizer.encode("-").tolist()[0]
 
         digits_tokens: List[int] = reduce(
             lambda ac, el: ac + el,
@@ -34,14 +38,25 @@ class IntState(State):
         string_end_sequences = Model.get_instance().string_end_sequences
 
         self._stage_allowed_tokens = {
-            IntStateStage.INITIAL: minus_token + digits_tokens,
-            IntStateStage.DATA: (
+            IntStateStage.INITIAL: self.minus_token + digits_tokens,
+            IntStateStage.POST_MINUS: digits_tokens,
+            IntStateStage.POST_DIGIT: (
                 digits_tokens + string_end_sequences
             ),
             IntStateStage.TERMINAL: None,
         }
 
         self._stage_transition_tokens = {
-            IntStateStage.INITIAL: digits_tokens,
-            IntStateStage.DATA: string_end_sequences,
+            IntStateStage.INITIAL: digits_tokens + self.minus_token,
+            IntStateStage.POST_MINUS: digits_tokens,
+            IntStateStage.POST_DIGIT: string_end_sequences,
         }
+
+    def _advance_stage(self, token: int) -> None:
+        if self.current_stage != IntStateStage.INITIAL:
+            super()._advance_stage(token)
+        else:
+            if token in self.minus_token:
+                self.current_stage = IntStateStage.POST_MINUS
+            else:
+                self.current_stage = IntStateStage.POST_DIGIT
